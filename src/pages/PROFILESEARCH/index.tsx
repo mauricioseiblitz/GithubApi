a508fa7cee44c70804c8173d1539fe9310a9bac0
Mauricio3 <mauricio-monteiro@hotmail.com.br>
2023-06-20 13:53:48 -0300

    Form Data

--- src/pages/PROFILESEARCH/index.tsx
+++ src/pages/PROFILESEARCH/index.tsx
@@ -1,20 +1,62 @@
 import { ReactComponent as MainImage } from 'assets/images/image.svg';
+import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import axios from 'axios';
 
 import './styles.css';
-import ResultCard from 'components/ResultCard';
+
+type FormData = {
+    login: string;
+}
+
+type Profile = {
+    url: string;
+    followers: string;
+    location: string;
+    name: string;
+}
 
 const ProfileSearch = () => {
+
+    const [profile, setProfile] = useState<Profile>();
+
+    const [formData, setFormData] = useState<FormData>({
+        login: '',
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const hanbleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        axios
+            .get(`https://api.github.com/users/${formData.login}`)
+            .then((response) => {
+                setProfile(response.data);
+            })
+            .catch((error) => {
+                setProfile(undefined);
+                console.log(error);
+            });
+    }
+
     return (
         <div className="profile-search-container">
             <div className="container search-container">
                 <h1>Encontre um perfil Github</h1>
-                <form>
+                <form onSubmit={hanbleSubmit}>
                     <div className="form-container">
                         <input
                             type="text"
+                            name="login"
                             className="search-input"
                             placeholder="Usuário Github"
-                            onChange={() => { }}
+                            value={formData.login}
+                            onChange={handleChange}
                         />
                         <button type="submit" className="btn btn-primary search-button">
                             Encontrar
@@ -28,14 +70,18 @@ const ProfileSearch = () => {
                 </div>
                 <div className="informacoes-container">
                     <h1>Informações</h1>
-                    <ResultCard title='Perfil: ' description='Perfil: https://api.github.com/users/acenelio' />
-                    <ResultCard title='Seguidores: ' description='4379' />
-                    <ResultCard title='Localidade: ' description='Uberlândia' />
-                    <ResultCard title='Nome: ' description='Nelio Alves' />
+                    {profile &&
+                        <>
+                            <ResultCard title='Perfil: ' description={profile.url} />
+                            <ResultCard title='Seguidores: ' description={profile.followers} />
+                            <ResultCard title='Localidade: ' description={profile.location} />
+                            <ResultCard title='Nome: ' description={profile.name} />
+                        </>
+                    }
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default ProfileSearch;
